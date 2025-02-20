06868c49d349764c109c20629cff180dea97287f
mariacosta2203 <maria.oliveiracosta13@gmail.com>
2025-02-20 08:17:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListarUsuarios from './components/usuarios/listarusuarios/ListarUsuarios'
+
+function App() {
+
+  return (
+    <>
+      <ListarUsuarios />
+    </>
+  )
+}
+
+export default App
